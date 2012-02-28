8a22c8110c25f36800b44a5e8de69ee267dc00b4
Whit Morriss <whit@surveymonkey.com>
2012-02-27 17:27:43 -0800

    move jinja2 include

--- doula/wsgiapp.py
+++ doula/wsgiapp.py
@@ -5,15 +5,14 @@ def main(global_config, **settings):
     with prism.configurator(appname=this,
                             global_config=global_config,
                             settings=settings) as config:
+        config.include('pyramid_jinja2')
         config.add_translation_dirs('locale/')
         config.scan('doula.views')
-    
-    # Routing for static files
-    config.add_static_view(name='js', path='static/js')
-    config.add_static_view(name='prodjs', path='static/prodjs')
-    config.add_static_view(name='css', path='static/css')
-    config.add_static_view(name='images', path='static/images')
-    config.add_static_view('static', 'static')
+        config.add_static_view(name='js', path='static/js')
+        config.add_static_view(name='prodjs', path='static/prodjs')
+        config.add_static_view(name='css', path='static/css')
+        config.add_static_view(name='images', path='static/images')
+        config.add_static_view('static', 'static')
     
     return config.wsgiapp
 
