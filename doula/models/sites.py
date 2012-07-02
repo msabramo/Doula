4745a34067b5a63c2e26c6101dd1815c6a4fe9f0
Joe Dallago <joed@surveymonkey.com>
2012-07-02 13:50:17 -0700

    Made the changes that were added in the  branch.

--- doula/models/sites.py
+++ doula/models/sites.py
@@ -3,10 +3,15 @@ import re
 import requests
 import logging
 import operator
+import shutil
 import traceback
 
+from fabric.api import *
+from tempfile import TemporaryFile
+from git import *
+from contextlib import contextmanager
+
 from doula.util import dirify
-from doula.util import dumps
 from doula.util import is_number
 from doula.models.audit import Audit
 from doula.models.sites_dal import SiteDAL
@@ -361,6 +366,63 @@ class Package(object):
     def __init__(self, name, version):
         self.name = name
         self.version = version
+
+    def distribute(self, branch, new_version):
+        with self.repo() as repo:
+            self.update_version(repo, new_version)
+            self.commit(repo, ['setup.py'], 'DOULA: Updating Version.')
+            self.push(repo, "origin")
+            self.upload(repo)
+
+    @contextmanager
+    def repo(self):
+        try:
+            # Where all of the cloned repos are placed
+            if not os.path.exists('repos'):
+                os.mkdir('repos')
+            repo_path = os.path.join('repos', self.name)
+
+            # Clone specified service's repo
+            repo = Repo.clone_from("git@code.corp.surveymonkey.com:joed/%s.git" % self.name,
+                                   repo_path)
+            yield repo
+        finally:
+            # Clean up repo directory
+            shutil.rmtree(repo.working_dir)
+
+    def update_version(self, repo, new_version):
+        # Alter setup.py to match the new version
+        setup_py_path = os.path.join(repo.working_dir, 'setup.py')
+        with open(setup_py_path, 'r+') as f:
+            tmp = TemporaryFile()
+
+            for line in f.readlines():
+                if line.startswith("version"):
+                    line = "version = '%s'\n" % new_version
+                tmp.write(line)
+
+            # Go back to the beginning of each file
+            tmp.seek(0)
+            f.seek(0)
+
+            f.write(tmp.read())
+
+    def commit(self, repo, files, msg):
+        # Commit change to repo
+        index = repo.index
+        index.add(files)
+        index.commit(msg)
+
+    def push(self, repo, remote):
+        # Push changes
+        origin = repo.remotes[remote]
+        origin.pull()
+        origin.push()
+
+    def upload(self, repo):
+        # Call `python setup.py sdist upload` to put upload to cheeseprism
+        with lcd(repo.working_dir):
+            local('python setup.py sdist upload -r local')
 
 
 class Tag(object):
