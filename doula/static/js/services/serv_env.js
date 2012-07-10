1b5ee738546689ab10135ed90a55b8f1e3457612
Alex V <alex.quez@gmail.com>
2012-07-10 15:11:31 -0700

    testing the queue, push to cheese task

--- doula/static/js/services/serv_env.js
+++ doula/static/js/services/serv_env.js
@@ -19,25 +19,78 @@ var ServiceEnv = {
 			$('#add-packages').modal();
 		});
 
-		$('.new-version-btn').on('click', _bind(this.showNewVersionModal, this));
+		$('.new-version-btn').on('click', _bind(this.showPushPackageModal, this));
 	},
 
-	showNewVersionModal: function(event) {
+	showPushPackageModal: function(event) {
 		var name = $(event.srcElement).attr('data-name');
-		console.log($(event.srcElement));
+
 		// make ajax requests to get modal
-		// /sites/{site_id}/{serv_id}/cheese_prism_modal
 		var url = '/sites/' + Data.site_name + '/';
 		url += Data.name_url + '/cheese_prism_modal';
-		this.get('pull', url, {'name': name}, this.doneShowNewVersionModal);
+
+		this.get('pull', url, {'name': name}, this.doneShowPushPackageModal);
 
 		return false;
 	},
 
-	doneShowNewVersionModal: function(rslt) {
+	doneShowPushPackageModal: function(rslt) {
+		$('#push-to-cheese-modal').on('shown', function() {
+			$('#push_package_branch')
+				.on('change', ServiceEnv.validateShowPushPackageModal);
+			$('#push_package_version')
+				.on('keyup', ServiceEnv.validateShowPushPackageModal)
+				.on('mouseup', ServiceEnv.validateShowPushPackageModal);
+
+			$('#push_package').on('click', _bind(ServiceEnv.pushPackage, ServiceEnv));
+		});
+
 		$('#push-to-cheese-modal')
 			.html(rslt)
 			.modal();
+	},
+
+	// alextodo, what about checking against an existing package number?
+	// have that happen on the backend
+	validateShowPushPackageModal: function() {
+		var branch = $.trim($('#push_package_branch').val());
+		var version = $.trim($('#push_package_version').val());
+
+		if (branch !== '' && version !== '') {
+			$('#push_package').removeClass('disabled');
+		}
+		else {
+			$('#push_package').addClass('disabled');
+		}
+	},
+
+	// alextodo setup the eventing system that notifies us of things happening
+	// on the backend. use the 10 lines of code for jquery events
+	pushPackage: function() {
+		var url = '/sites/' + Data.site_name + '/';
+		url += Data.name_url + '/cheese_prism_push';
+
+		var params = {
+			'name': $('#push_package_name').val(),
+			'branch': $('#push_package_branch').val(),
+			'next_version': $('#push_package_version').val()
+		};
+
+		// alextodo, will need to properly handle errors from backend. show in modal
+		this.get('Pushing package to Cheese Prism', url, params, this.donePushPackage, this.failedPushPackage);
+	},
+
+	donePushPackage: function(rslt) {
+		console.log(rslt.job);
+		$('#push-to-cheese-modal').modal('hide');
+		// after this call make sure to update the UI, things need to be freezed
+	},
+
+	failedPushPackage: function(rslt) {
+		// update the error messages in modal
+		console.log('failed push package');
+		console.log(rslt);
+
 	}
 };
 
