d23ad77754175316cded832c63046bf69a9960e5
Joe Dallago <joed@surveymonkey.com>
2012-07-19 15:11:42 -0700

    Converted everything to the new exception handling mechanism.

--- doula/views/index.py
+++ doula/views/index.py
@@ -4,7 +4,6 @@ from doula.jobs_timer import start_task_scheduling
 from doula.models.sites_dal import SiteDAL
 from doula.util import *
 from doula.views.helpers import *
-from git import GitCommandError
 from pyramid.events import ApplicationCreated
 from pyramid.events import subscriber
 from pyramid.response import FileResponse
@@ -28,21 +27,13 @@ def show_sites(request):
     """
     Show the testing sites
     """
-    try:
-        sites = SiteDAL.get_sites()
-    except Exception as e:
-        return handle_exception(e, request)
-
+    sites = SiteDAL.get_sites()
     return {'sites': sites, 'config': Config}
 
 
 @view_config(route_name='site', renderer="sites/site.html")
 def site(request):
-    try:
-        site = get_site(request.matchdict['site_id'])
-    except Exception as e:
-        return handle_exception(e, request)
-
+    site = get_site(request.matchdict['site_id'])
     return {
         'site': site,
         'site_json': dumps(site),
@@ -53,7 +44,6 @@ def site(request):
 
 @view_config(route_name='site_tag', renderer="string")
 def site_tag(request):
-    try:
         tag_history_path = Config.get('tag_history_path')
         tag_history_remote = Config.get('tag_history_remote')
         tag = git_dirify(request.POST['tag'])
@@ -62,13 +52,7 @@ def site_tag(request):
         site = get_site(request.POST['site_id'])
         # once we have user from ldap, pass that in
         site.tag(tag_history_path, tag_history_remote, tag, msg, 'anonymous')
-
         return dumps({'success': True, 'site_id': site})
-    except GitCommandError as e:
-        msg = "Git error: {0}." .format(str(e.stderr))
-        return handle_json_exception(e, msg, request)
-    except Exception as e:
-        return handle_json_exception(e, msg, request)
 
 
 # BAMBINO VIEWS
@@ -94,13 +78,8 @@ def bambino_ips(request):
     Return all the IP addresses for the Bambinos.
     Used for deployment to update every Bambino registered with Doula.
     """
-    try:
-        ips = SiteDAL.get_node_ips()
-        return {'success': True, 'ip_addresses': ips}
-    except KeyError as e:
-        msg = 'Unable to find Bambino IP addresses "{0}"'
-        msg = msg.format(request.POST['site_id'], request.POST['serv_id'])
-        return handle_json_exception(e, msg, request)
+    ips = SiteDAL.get_node_ips()
+    return {'success': True, 'ip_addresses': ips}
 
 
 ####################
--- doula/views/services.py
+++ doula/views/services.py
@@ -18,16 +18,9 @@ log = logging.getLogger(__name__)
 
 @view_config(route_name='service', renderer="services/release_actions.html")
 def service(request):
-    try:
-        site = get_site(request.matchdict['site_id'])
-        service = site.services[request.matchdict['serv_id']]
-        other_packages = CheesePrism.other_packages(service.packages)
-    except Exception:
-        msg = 'Unable to find site and service under "{0}" and "{1}"'
-        msg = msg.format(request.matchdict['site_id'], request.matchdict['serv_id'])
-
-        raise HTTPNotFound(msg)
-
+    site = get_site(request.matchdict['site_id'])
+    service = site.services[request.matchdict['serv_id']]
+    other_packages = CheesePrism.other_packages(service.packages)
     return {
         'site': site,
         'service': service,
@@ -39,19 +32,14 @@ def service(request):
 
 @view_config(route_name='service_cheese_prism_modal', renderer="services/modal_push_package.html")
 def service_cheese_prism_modal(request):
-    try:
-        site = get_site(request.matchdict['site_id'])
-        service = site.services[request.matchdict['serv_id']]
-        package = service.get_package_by_name(request.GET['name'])
+    site = get_site(request.matchdict['site_id'])
+    service = site.services[request.matchdict['serv_id']]
+    package = service.get_package_by_name(request.GET['name'])
 
-        versions = package.get_versions()
-        versions.sort()
-        versions.reverse()
-        current_version = versions[0]
-    except Exception as e:
-        msg = 'Error pulling Push New Package Modal'
-        return handle_json_exception(e, msg, request)
-
+    versions = package.get_versions()
+    versions.sort()
+    versions.reverse()
+    current_version = versions[0]
     return {
         'service': service,
         'package': package,
@@ -62,23 +50,19 @@ def service_cheese_prism_modal(request):
 
 @view_config(route_name='service_cheese_prism_push', renderer="string")
 def service_cheese_prism_push(request):
-    try:
-        site = get_site(request.matchdict['site_id'])
-        service = site.services[request.matchdict['serv_id']]
-        package = service.get_package_by_name(request.GET['name'])
+    raise Exception('blah')
+    site = get_site(request.matchdict['site_id'])
+    service = site.services[request.matchdict['serv_id']]
+    package = service.get_package_by_name(request.GET['name'])
 
-        # alextodo, use a validation for the branch and version right here
-        # no empty values, no duplicates, make sure the branch exist
-        # alextodo, verification will also require that we don't allow
-        # another version number that is already being pushed onto the queue
-        remote = package.get_github_info()['ssh_url']
-        next_version = request.GET['next_version']
-        branch = request.GET['branch']
-        job_dict = enqueue_push_package(service, remote, branch, next_version)
-    except Exception as e:
-        msg = 'Error pulling Push New Package Modal'
-        return handle_json_exception(e, msg, request)
-
+    # alextodo, use a validation for the branch and version right here
+    # no empty values, no duplicates, make sure the branch exist
+    # alextodo, verification will also require that we don't allow
+    # another version number that is already being pushed onto the queue
+    remote = package.get_github_info()['ssh_url']
+    next_version = request.GET['next_version']
+    branch = request.GET['branch']
+    job_dict = enqueue_push_package(service, remote, branch, next_version)
     return dumps({'success': True, 'job': job_dict})
 
 
@@ -101,52 +85,34 @@ def enqueue_push_package(service, remote, branch, version):
 
 @view_config(route_name='service_details', renderer="services/service_details.html")
 def service_details(request):
-    try:
-        site = get_site(request.matchdict['site_id'])
-        service = site.services[request.matchdict['serv_id']]
-
-    except Exception:
-        msg = 'Unable to find site and service under "{0}" and "{1}"'
-        msg = msg.format(request.matchdict['site_id'], request.matchdict['serv_id'])
-
-        raise HTTPNotFound(msg)
-
+    site = get_site(request.matchdict['site_id'])
+    service = site.services[request.matchdict['serv_id']]
     return {'site': site, 'service': service, 'config': Config}
 
 
 @view_config(route_name='service_tag', renderer="string")
 def service_tag(request):
-    try:
-        service = SiteDAL.get_service(request.matchdict['site_id'], request.matchdict['serv_id'])
-        # todo, once we have a user logged in we'll pass in the user too
-        tag = git_dirify(request.POST['tag'])
-        service.tag(tag, request.POST['msg'], 'anonymous')
+    service = SiteDAL.get_service(request.matchdict['site_id'], request.matchdict['serv_id'])
+    # todo, once we have a user logged in we'll pass in the user too
+    tag = git_dirify(request.POST['tag'])
+    service.tag(tag, request.POST['msg'], 'anonymous')
 
-        # pull the updated service
-        updated_service = SiteDAL.get_service(request.matchdict['site_id'], request.matchdict['serv_id'])
+    # pull the updated service
+    updated_service = SiteDAL.get_service(request.matchdict['site_id'], request.matchdict['serv_id'])
 
-        return dumps({'success': True, 'service': updated_service})
-    except Exception as e:
-        msg = 'Unable to tag site and service under "{0}" and "{1}"'
-        msg = msg.format(request.POST['site_id'], request.POST['serv_id'])
-        return handle_json_exception(e, msg, request)
+    return dumps({'success': True, 'service': updated_service})
 
 
 @view_config(route_name='service_deploy', renderer="string")
 def service_deploy(request):
-    try:
-        validate_token(request)
+    validate_token(request)
 
-        service = SiteDAL.get_service(SiteDAL.get_master_site(), request.POST['serv_id'])
-        tag = service.get_tag_by_name(request.POST['tag'])
-        # todo, for now pass in the anonymous user until we start authenticating
-        service.mark_as_deployed(tag, 'anonymous')
+    service = SiteDAL.get_service(SiteDAL.get_master_site(), request.POST['serv_id'])
+    tag = service.get_tag_by_name(request.POST['tag'])
+    # todo, for now pass in the anonymous user until we start authenticating
+    service.mark_as_deployed(tag, 'anonymous')
 
-        return dumps({'success': True, 'service': service})
-    except Exception as e:
-        msg = 'Unable to deploy service. Error: "{0}"'
-        msg = msg.format(e.message)
-        return handle_json_exception(e, msg, request)
+    return dumps({'success': True, 'service': service})
 
 
 def validate_token(request):
@@ -157,16 +123,12 @@ def validate_token(request):
 
 @view_config(route_name="service_freeze")
 def service_freeze(request):
-    try:
-        site = get_site(request.matchdict['site_id'])
-        service = site.services[request.matchdict['serv_id']]
+    site = get_site(request.matchdict['site_id'])
+    service = site.services[request.matchdict['serv_id']]
 
-        response = Response(content_type='service/octet-stream')
-        file_name = service.site_name + '_' + service.name_url + '_requirements.txt'
-        response.content_disposition = 'attachment; filename="' + file_name + '"'
-        response.charset = "UTF-8"
-        response.text = service.freeze_requirements()
-    except Exception as e:
-        return handle_exception(e, request)
-
+    response = Response(content_type='service/octet-stream')
+    file_name = service.site_name + '_' + service.name_url + '_requirements.txt'
+    response.content_disposition = 'attachment; filename="' + file_name + '"'
+    response.charset = "UTF-8"
+    response.text = service.freeze_requirements()
     return response
