a0ed66db36c1b4a17d2c0339a3e2d5b8ce6f3fbb
Alex V <alex.quez@gmail.com>
2012-10-16 17:37:54 -0700

    removing old search_queue code

--- doula/views/queue.py
+++ doula/views/queue.py
@@ -15,74 +15,6 @@ def show_queue(request):
     }
 
 
-def search_queue(request):
-    """
-    Search for the latest jobs on the queue
-    """
-    service = get_service_for_query(request)
-    jobs_started_after = int(request.params.getone('jobs_started_after'))
-    job_ids = json.loads(request.params.getone('job_ids'))
-
-    jobs = []
-
-    for job in query_queue(request):
-        # Only return the job if a new job (the start time is after the
-        # jobs_started_after time value) or the job is listed in the
-        # jobs_and_statuses dictionary
-        if job['time_started'] > jobs_started_after or job['id'] in job_ids:
-            if service == False or job['service'] == service:
-                # If job is already completed/failed
-                if job['status'] in ['complete', 'failed']:
-                    job['log'] = get_log(job['id'])
-
-                # render the job
-                # alextodo. change the queue_item to job everywhere.
-                job['html'] = render('doula:templates/queue/queued_item.html',
-                                      {'queued_item': job})
-
-                jobs.append(job)
-
-    # sort jobs with respect to time
-    jobs = sorted(jobs, key=lambda k: k['time_started'])
-
-    return {
-        'success': True,
-        'jobs': jobs
-    }
-
-
-def get_service_for_query(request):
-    if request.params.getone('service'):
-        return request.params.getone('service')
-    else:
-        return False
-
-
-def query_queue(request):
-    query = {
-        'job_type': [
-            'push_to_cheeseprism',
-            'cycle_services',
-            'push_service_environment']
-        }
-
-    if not request.params.getone('filter_by') == 'alljobs':
-        query['user_id'] = request.user['username']
-
-    if request.params.getone('sort_by'):
-        if request.params.getone('sort_by') != 'all':
-            query['status'] = request.params.getone('sort_by')
-
-    if request.params.getone('site'):
-        query['site'] = request.params.getone('site')
-
-    if request.params.getone('service'):
-        query['service'] = request.params.getone('service')
-
-    return Queue().get(query)
-
-
-# New way to query the queue
 @view_config(route_name='queue', request_param='jobs_started_after', renderer='json', xhr=True)
 def query_queue_view(request):
     response = {'success': True}
@@ -104,7 +36,7 @@ def query_queue_view(request):
                 job['log'] = get_log(job['id'])
 
             # render the job
-            # alextodo. change the queue_item to job everywhere.
+            # todo. change the queue_item to job everywhere.
             job['html'] = render('doula:templates/queue/queued_item.html',
                                   {'queued_item': job})
 
