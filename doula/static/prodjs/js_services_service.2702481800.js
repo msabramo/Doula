8d7c75abb8cf6ad4df8c0cc48c452e8bb0e5ec34
Alex V <alex.quez@gmail.com>
2012-11-10 11:26:00 -0800

    updating js

--- doula/static/prodjs/js_services_service.2702481800.js
+++ doula/static/prodjs/js_services_service.2702481800.js
@@ -6,6 +6,8 @@ return arrayWithout;};var AJAXUtil={post:function(url,params,onDone,onFail,msg){
 else{AJAXUtil._showStandardErrorMessage(rslt);}},success:function(response){try{if(msg){setTimeout(function(){Notifier.hide();},1500);}
 rslt=(typeof(response)=='string')?$.parseJSON(response):response;if(rslt.success){onDone(rslt);}
 else{if(typeof(onFail)=='function'){onFail(rslt);}
-else{AJAXUtil._showStandardErrorMessage(rslt);}}}
+else{Notifier.hide();AJAXUtil._showStandardErrorMessage(rslt);}}}
 catch(err){onDone(response);}}});},_getDataValues:function(params){var dataValues='';var count=0;for(var key in params){if(dataValues!=='')dataValues+='&';dataValues+=key+'='+encodeURIComponent(params[key]);}
-return dataValues;},_showStandardErrorMessage:function(rslt){$('#modal-error').on('show',function(){$('#modal-error-msg').html(rslt.msg);$('#modal-error-close').on('click',function(){$('#modal-error').modal('hide');return false;});});$('#modal-error').modal();}};
+return dataValues;},_showStandardErrorMessage:function(rslt){$('#modal-error').on('show',function(){$('#modal-error-msg').html(rslt.msg);$('#modal-error-close').on('click',function(){$('#modal-error').modal('hide');return false;});});$('#modal-error').modal();}};;var Service={compareURL:'',init:function(){this.compareURL=__compare_url;this.bindToUIActions();},bindToUIActions:function(){$('#compare_dropdown1,#compare_dropdown2').bind('click change blur keydown',$.proxy(this.updateCompareURL,this));this.updateCompareURL();},updateCompareURL:function(){var tag1=$('#compare_dropdown1').val();var tag2=$('#compare_dropdown2').val();var urlArray=this.compareURL.split('/');var url='';for(var i=0;i<urlArray.length-1;i++){url+=urlArray[i]+'/';}
+url+=tag1+'...'+tag2;if(this.compareURL){$('#compare_url').attr('href',url);$('#compare_url').html('View differences between "'+tag1+'" and "'+tag2+'".');}
+else{$('#compare_url').attr('href',"http://code.corp.surveymonkey.com/");$('#compare_url').html('Comparison not available for this service.');}}};$(document).ready(function(){Service.init();});;
