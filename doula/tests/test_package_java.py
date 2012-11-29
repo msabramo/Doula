d7776a0cb3ee92fef2ee598de155aaf4a2f04350
Timothy Sabat <tims@surveymonkey.com>
2012-11-28 18:52:20 -0800

    more java stuff

---
+++ doula/tests/test_package_java.py
@@ -0,0 +1,42 @@
+from doula.config import Config
+from doula.models.package_java import PackageJava
+from mock import call
+from mock import patch
+import os
+import uuid
+import unittest
+
+
+class PackageJavaTests(unittest.TestCase):
+    def setUp(self):
+        settings = {
+            'doula.cheeseprism_url': 'http://yorick:9003',
+            'doula.cheeseprism_package_index_path': '/var/www/python-pkgindex',
+            'doula.keyfile_path': '~/.ssh/id_rsa_doula'
+        }
+        Config.load_config(settings)
+
+    def testDown(self):
+        pass
+
+    def make_one(self):
+        return PackageJava("userdal", "0.1.3", "git@code.corp.surveymonkey.com:tbone")
+
+    def test_distribute(self):
+        job_dict = {
+            'version': str(uuid.uuid1()),
+            'user_id': 'alexv',
+            'package_name': 'userdal',
+            'service': 'userdal',
+            'branch': 'master',
+            'remote': 'git@code.corp.surveymonkey.com:tbone/userdal.git',
+            'job_type': 'build_new_package',
+            'site': 'alexs-macbook-pro-4.local',
+            'id': 'bc255ebaf6da11e1b07fb8f6b1191577'
+        }
+
+        p = PackageJava(job_dict['package_name'], '9.9.9', job_dict['remote'])
+        p.distribute(job_dict['branch'], job_dict['version'])
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ doula/tests/test_push_java.py
@@ -0,0 +1,32 @@
+import os
+import unittest
+from doula.models.push_java import PushJava
+from doula.config import Config
+
+class PushTestsJava(unittest.TestCase):
+
+    def setUp(self):
+        settings = {
+            'doula.cheeseprism_url': 'http://yorick:9003',
+            'doula.cheeseprism_package_index_path': '/var/www/python-pkgindex',
+            'doula.keyfile_path': '~/.ssh/id_rsa_doula'
+        }
+        Config.load_config(settings)
+
+    def test_push(self):
+
+        job_dict = {
+            'id': 111,
+            'user_id': 'tbone',
+            'service_name': 'createweb',
+            'site_name_or_node_ip': 'mt-99',
+            'packages': ['requests']
+        }
+
+        service = PushJava('userdal', 'vagrant', '/opt/java', 'http://yorick:9003',
+                '~/.ssh/id_rsa_doula', '33.33.33.10', True)
+
+        service.release_service(['UserAccount.war_9.9.9'])
+
+if __name__ == '__main__':
+    unittest.main()
