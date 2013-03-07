4a508ec3655228d277460b279a7529f8d093b96e
Marc Abramowitz <marca@surveymonkey.com>
2013-03-07 14:52:31 -0800

    Wrap long line on to multiple lines in doula/tests/test_push.py

--- doula/tests/test_push.py
+++ doula/tests/test_push.py
@@ -29,7 +29,10 @@ class PushTests2(unittest.TestCase):
         push.fabric_user = Mock(return_value=u)
         push._chown = Mock()
         push.install_assets = Mock(return_value=(True, True))
-        push.packages_to_node({'packages': {'requests': '0.13.0'}, 'is_rollback': True, 'etc_sha1': '93f481d56d3e3e7c939bc172507106d76ed546f1'})
+        push.packages_to_node(
+            {'packages':     {'requests': '0.13.0'},
+             'is_rollback':  True,
+             'etc_sha1':     '93f481d56d3e3e7c939bc172507106d76ed546f1'})
 
 if __name__ == '__main__':
     unittest.main()
